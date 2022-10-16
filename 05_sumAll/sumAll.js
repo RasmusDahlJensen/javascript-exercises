0cb85d5719d2d083a341f1c756af7d52d0561609
Rasmus Jensen <rasmusdj96@gmail.com>
2022-10-16 13:15:53 +0200

    123

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
 const sumAll = function (startNum, endNum) {
+	if (
+		startNum < 0 ||
+		endNum < 0 ||
+		typeof startNum != "number" ||
+		typeof endNum != "number"
+	) {
+		return "ERROR";
+	} else if (startNum > endNum) {
+		let sum = 0;
+		for (let i = endNum; i <= startNum; i++) {
+			sum += i;
+		}
+		return sum;
+	}
 	let sum = 0;
 	for (let i = startNum; i <= endNum; i++) {
 		sum += i;
