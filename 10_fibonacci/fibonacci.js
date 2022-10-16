18d2fe659736f96f3cdb4ad9c5e372abd70bcbd2
Rasmus Jensen <rasmusdj96@gmail.com>
2022-10-16 15:17:53 +0200

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	if (num < 0) {
+		return "OOPS";
+	}
+	if (num == 1) {
+		return 1;
+	} else {
+		let a = 0;
+		let b = 1;
+		let sum;
+		for (let i = 1; i < +num; i++) {
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+		return sum;
+	}
 };
 
 // Do not edit below this line
