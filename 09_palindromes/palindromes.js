ccae917023b3d8397f43dad2c1d4132b0a00b7fc
Rasmus Jensen <rasmusdj96@gmail.com>
2022-10-16 14:52:16 +0200

    palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+	let original = word
+		.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+		.replace(/\s{2,}/g, " ")
+		.replace(/\s+/g, "");
+	console.log("original", original);
 
+	string = word.split("").reverse().join("");
+	let newString = string
+		.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+		.replace(/\s{2,}/g, " ")
+		.replace(/\s+/g, "");
+	console.log("Reversed", newString);
+
+	return newString.toLowerCase() == original.toLowerCase();
 };
 
 // Do not edit below this line
